eadbccf5d3474669d25125c2ac0e7902b51905cf
Bryan Peabody <bryan.peabody@bhsi.com>
2019-06-28 15:55:46 -0400

    Initial add.

---
+++ peanuts.py
@@ -0,0 +1,41 @@
+import datetime
+from datetime import timedelta
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+import giphypop
+from urllib import request
+
+start_date = datetime.datetime(1950, 10, 2)
+end_date = datetime.datetime(1950, 11, 1)
+#end_date = datetime.datetime(2000, 1, 3)
+
+# Loop over all the date range
+while start_date <= end_date:
+    filename = start_date.strftime('%Y-%m-%d') + ".gif"    
+    url = "https://www.gocomics.com/peanuts/" + start_date.strftime('%Y') + "/" + start_date.strftime('%m') + "/" + start_date.strftime('%d')
+
+    # Read in the HTML from the URL
+    fp = urllib.request.urlopen(url)
+    mybytes = fp.read()
+
+    peanutsHTML = mybytes.decode("utf8")
+    fp.close()
+
+    # Setup parser
+    soup = BeautifulSoup(peanutsHTML, 'html.parser')
+
+    # Filter to the picture tag and specific class
+    picture = soup.find("picture", {"class": "item-comic-image"})
+
+    # Extract the img tag
+    img = picture.img
+
+    # Get the image src - will be the comic strip as a gif
+    comicStripURL = img['src']
+
+    g = giphypop.Giphy()
+    request.urlretrieve(comicStripURL, filename)
+
+    # Increment the date
+    start_date += timedelta(days=1)
