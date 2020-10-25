0f84c6ff14f77efea5ec4afd18275e05355827d7
Bryan Peabody <hi@bryanpeabody.com>
2020-10-25 14:37:07 -0400

    Updated python magic.

--- peanuts.py
+++ peanuts.py
@@ -9,10 +9,17 @@ import os
 from sys import argv
 
 # Get the start and end dates from the passed in parameters
-start_date = datetime.strptime(argv[1], '%Y-%m-%d')
-end_date = datetime.strptime(argv[2], '%Y-%m-%d')
+start_date = ""
+end_date = ""
 
-# Loop over all the date range
+try :
+    start_date = datetime.strptime(argv[1], '%Y-%m-%d')
+    end_date = datetime.strptime(argv[2], '%Y-%m-%d')
+except:
+    print("Please pass in valid start and end date parameters! ")
+    exit(-1)
+
+# Start and end dates were good, lets grab each comic strip for the date range
 while start_date <= end_date:
     filename = "./images/" + start_date.strftime('%Y-%m-%d')  
     url = "https://www.gocomics.com/peanuts/" + start_date.strftime('%Y') + "/" + start_date.strftime('%m') + "/" + start_date.strftime('%d')
@@ -31,7 +38,6 @@ while start_date <= end_date:
     picture = soup.find("picture", {"class": "item-comic-image"})
 
     # Extract the img tag
-    #try:            
     img = picture.img                
 
     # Get the image src - will be the comic strip as a gif
@@ -39,8 +45,8 @@ while start_date <= end_date:
     request.urlretrieve(comicStripURL, filename)
 
     # Determine the mime type. Either jpg or gif
-    mime = magic.Magic(mime=True) 
-    mimetype = mime.from_file(filename)   
+    mime = magic.Magic(mime=True)
+    mimetype = mime.from_file(filename)
     
     ext = '.'
 
@@ -53,9 +59,10 @@ while start_date <= end_date:
 
     # Rename the file
     os.rename(filename, filename + ext)
-    #except Exception as err:        
-    #    print("Failed to get: "+ filename)
 
     # Increment the date
     start_date += timedelta(days=1)
+
+# Finished
+print("Finished. See the /images directory for the comic strips.")
 
